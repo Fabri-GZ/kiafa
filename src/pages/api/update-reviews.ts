ac472c27731886987e40b0cf1b1b54cb7fbd0201
Fabrizio <fabriziovguzman@gmail.com>
2025-12-23 20:09:25 -0300

    a

--- src/pages/api/update-reviews.ts
+++ src/pages/api/update-reviews.ts
@@ -4,7 +4,8 @@ import { kv } from "@vercel/kv";
 
 export const GET: APIRoute = async ({ request }) => {
   const authHeader = request.headers.get("authorization");
-
+  console.log("AUTH HEADER:", request.headers.get("authorization"));
+  console.log("ENV SECRET:", import.meta.env.CRON_SECRET);
   if (authHeader !== `Bearer ${import.meta.env.CRON_SECRET}`) {
     console.log("Unauthorized");
     return new Response("Unauthorized", { status: 401 });
